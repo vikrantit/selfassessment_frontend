3ed924c232869a44bb7089f4aa1d343c5ee7d726
Subhankar1909 <31899275+Subhankar1909@users.noreply.github.com>
2020-04-07 20:53:57 -0400

    made changes in build

---
+++ build/precache-manifest.c9f43da27a1c1a35b3f8de1b0b255753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be26467e0de6f4946641b8234fd9eed4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e3745f7d52ad102cf8af",
+    "url": "/my-app/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e8c766baad91c427c3e2",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e3745f7d52ad102cf8af",
+    "url": "/my-app/static/js/2.525fc905.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.525fc905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c766baad91c427c3e2",
+    "url": "/my-app/static/js/main.694f583b.chunk.js"
+  },
+  {
+    "revision": "9ae7b24bd3dc121eea03",
+    "url": "/my-app/static/js/runtime-main.63080f57.js"
+  }
+]);
