653656f74d36f80ab96f2fa43292bec3e15f46bd
Subhankar1909 <31899275+Subhankar1909@users.noreply.github.com>
2020-04-13 17:44:49 -0400

    added more features after login

--- src/components/logintrial.js
+++ src/components/logintrial.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Figure from "react-bootstrap/Figure";
 
 const LoginTrial = () => {
   const [values, setValues] = useState({
@@ -119,36 +121,161 @@ const LoginTrial = () => {
     );
   };
 
-  const redirectpage= () => {
-    var url= "https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection/prevention-risks.html";
-    window.location= url;
+  const redirectpage = () => {
+    var url =
+      "https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection/prevention-risks.html";
+    window.location = url;
+  };
 
+  const nurseprofile = () => {
+    return (
+      <div className="row">
+        <div className="col-sm">
+          <Card style={{ width: "18rem" }}>
+            <Figure.Image
+              width={300}
+              height={200}
+              alt="300X200"
+              src="https://images.pexels.com/photos/590041/pexels-photo-590041.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            />
+            <Card.Body>
+              <Card.Title>Add Report</Card.Title>
+              <Card.Text>Add a new analysis of your patient</Card.Text>
+            </Card.Body>
+            <Button variant="primary" onClick={ ()=> {window.location="/reportnurse"}}>New Report</Button>
+          </Card>
+        </div>
+        <div className="col-sm">
+          <Card style={{ width: "18rem" }}>
+            <Figure.Image
+              width={300}
+              height={200}
+              alt="300X200"
+              src="https://images.pexels.com/photos/3584994/pexels-photo-3584994.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            />
+            <Card.Body>
+              <Card.Title>Search</Card.Title>
+              <Card.Text>Search the history of your patients</Card.Text>
+            </Card.Body>
+            <Button variant="primary" onClick={ ()=> {window.location="/searchallreports"}}>Search</Button>
+          </Card>
+        </div>
+        <div className="col-sm">
+          <Card style={{ width: "18rem" }}>
+            <Figure.Image
+              width={300}
+              height={200}
+              alt="200X200"
+              src="https://images.pexels.com/photos/163066/desk-terminplanung-control-newsletter-163066.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            />
+            <Card.Body>
+              <Card.Title>Tip</Card.Title>
+              <Card.Text>Add a new tip for your patients</Card.Text>
+            </Card.Body>
+            <Button variant="primary" onClick={ ()=> {window.location="/addTips"}} >Add Tip</Button>
+          </Card>
+        </div>
+      </div>
+    );
+  };
+
+  const patientprofile= () => {
+    return (
+      <div className="row">
+        <div className="col-sm">
+          <Card style={{ width: "18rem" }}>
+            <Figure.Image
+              width={300}
+              height={200}
+              alt="300X200"
+              src="https://images.pexels.com/photos/590041/pexels-photo-590041.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            />
+            <Card.Body>
+              <Card.Title>Add Report</Card.Title>
+              <Card.Text>Add a new analysis for yourself</Card.Text>
+            </Card.Body>
+            <Button variant="primary" onClick={ ()=> {window.location="/reportuser"}}>New Report</Button>
+          </Card>
+        </div>
+        <div className="col-sm">
+          <Card style={{ width: "18rem" }}>
+            <Figure.Image
+              width={300}
+              height={200}
+              alt="300X200"
+              src="https://images.pexels.com/photos/3970332/pexels-photo-3970332.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            />
+            <Card.Body>
+              <Card.Title>Emergency</Card.Title>
+              <Card.Text>Call 911 or Click Below</Card.Text>
+            </Card.Body>
+            <Button variant="danger" onClick={ ()=> {window.location="/emergency"}}>Emergency</Button>
+          </Card>
+        </div>
+        <div className="col-sm">
+          <Card style={{ width: "18rem" }}>
+            <Figure.Image
+              width={300}
+              height={200}
+              alt="200X200"
+              src="https://images.pexels.com/photos/40568/medical-appointment-doctor-healthcare-40568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            />
+            <Card.Body>
+              <Card.Title>Self Diagnosis</Card.Title>
+              <Card.Text>Run your symptoms here</Card.Text>
+            </Card.Body>
+            <Button variant="primary" onClick={ ()=> {window.location="/reportuser"}}>Diagnosis</Button>
+          </Card>
+        </div>
+      </div>
+    );
   }
+
   return (
-    <div>
+    <div >
       {user === null ? (
         <div>
-          {signInForm()} <br></br>{errorMessage()}
+          {signInForm()} <br></br>
+          {errorMessage()}
         </div>
       ) : (
-        <div>
-         
+        <div >
           <Jumbotron>
-            <h3 className="text-secondary" > Welcome {user.firstName} {user.lastName} </h3>
+            <h3 className="text-secondary">
+              {" "}
+              Welcome {user.firstName} {user.lastName}{" "}
+            </h3>
             <p>How are you feeling today?</p>
-              <p className="text-info">
-              Human coronaviruses cause infections of the nose, throat and lungs. They are most commonly spread from an infected person through:
-              </p>
-              <p className="text-danger">respiratory droplets generated when you cough or sneeze</p>
-              <p className="text-danger">close, prolonged personal contact, such as touching or shaking hands</p>
-              <p className="text-danger">touching something with the virus on it, then touching your mouth, nose or eyes before washing your hands</p>
+            <p className="text-info">
+              Human coronaviruses cause infections of the nose, throat and
+              lungs. They are most commonly spread from an infected person
+              through:
+            </p>
+            <p className="text-danger">
+              respiratory droplets generated when you cough or sneeze
+            </p>
+            <p className="text-danger">
+              close, prolonged personal contact, such as touching or shaking
+              hands
+            </p>
+            <p className="text-danger">
+              touching something with the virus on it, then touching your mouth,
+              nose or eyes before washing your hands
+            </p>
 
-              <form >
-                <Button variant="primary" onClick={redirectpage}>Learn more</Button>
-              </form>
+            <form>
+              <Button variant="primary" onClick={redirectpage}>
+                Learn more
+              </Button>
+            </form>
           </Jumbotron>
+          <div className="container">
+          {(user.role === 'Nurse')  ? <div className="row">{nurseprofile()}</div> : <div className="row">{patientprofile()}</div>}
+          </div>
         </div>
       )}
+
+     
     </div>
   );
 };
